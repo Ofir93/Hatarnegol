f8c521a1d3a5f5ddaed1061b93519164c3199553
Ofir <109758639+Ofir93@users.noreply.github.com>
2024-05-25 12:41:29 +0300

    deleting contact us section

--- index.js
+++ index.js
@@ -36,20 +36,11 @@ const plateMenuT = () => {
   plateMenu.classList.add('shows')
 }
 
-// const mealMenuT = () => {
-//   mainMenu.classList.add('hides')
-//   mealMenu.classList.add('shows')
-// }
-
 const drinkMenuT = () => {
   mainMenu.classList.add('hides')
   drinkMenu.classList.add('shows')
 }
 
-// const alcoholMenuT = () => {
-//   mainMenu.classList.add('hides')
-//   alcoholMenu.classList.add('shows')
-// }
 
 const marketMenuT = () => {
   mainMenu.classList.add('hides')
@@ -57,20 +48,11 @@ const marketMenuT = () => {
 }
 
 const backToggle = () => {
-  // console.log('object')
   smallMenu.classList.remove('shows')
   marketMenu.classList.remove('shows')
-  // alcoholMenu.classList.remove('shows')
   drinkMenu.classList.remove('shows')
-  // mealMenu.classList.remove('shows')
   plateMenu.classList.remove('shows')
   burgerMenu.classList.remove('shows')
   mainMenu.classList.remove('hides')
-  // mainMenu.classList.replace('hides', 'shows')
 }
 
-// $("button").click(function() {
-//   $('html,body').animate({
-//       scrollTop: $(".second").offset().top},
-//       'slow');
-// });
